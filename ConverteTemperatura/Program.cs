0f2cc028d2afc7e8ee1bb34dea5f9e3ced6736d2
maikeliani <maikel.iani31@gmail.com>
2023-03-03 14:37:10 -0300

    Add project files.

---
+++ ConverteTemperatura/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+double c, f;
+Console.WriteLine( " Informe a temperatura");
+c =double.Parse(Console.ReadLine());
+
+f = ((c * 9 / 5) + 32);
+Console.WriteLine(  " A temperatura em graus celsius é : " + c + "" +
+    " e a temperatura em graus farenheit é: " + f);
